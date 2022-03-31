6f7c37a48f3ade2198b7e0dbbbfd097a2966286e
youone04 <yudi.gunaone87@gmail.com>
2022-03-31 16:35:57 +0700

    update

--- index3.php
+++ index3.php
@@ -9,7 +9,7 @@ include('controller/function.php');
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+      
         <style>
             #customers {
             font-family: Arial, Helvetica, sans-serif;
